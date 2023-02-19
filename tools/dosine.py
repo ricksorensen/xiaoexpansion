c109f1cafe67a5c813029557ee80eceef93ce905
Rick Sorensen <rick.sorensen@charter.net>
2023-02-19 09:38:38 -0600

    dosine: turn off DotStar

--- tools/dosine.py
+++ tools/dosine.py
@@ -3,7 +3,36 @@ from math import sin, pi
 import time
 
 
+class simpleDotStar:
+    # create the SPI object. miso can be any unused pin.
+    # trinket M0 ds_clk=PA01, ds_data=PA00
+    #            PA05 unused
+    def __init__(
+        self,
+        clk=machine.Pin("PA01"),
+        data=machine.Pin("PA00"),
+        unused=machine.Pin("PA05"),
+        turnoff=False,
+    ):
+        self._ds = machine.SoftSPI(sck=clk, mosi=data, miso=unused)
+        if turnoff:
+            self.write()  # turn off
+
+    def write(self, *, red=0, green=0, blue=0):
+        self._ds.write(b"\x00\x00\x00\x00\xff")
+        self._ds.write(bytearray((blue, green, red)))
+        self._ds.write(b"\xff\xff\xff")
+
+    def deinit(self):
+        self._ds.deinit()
+
+    def init(self):
+        self._ds.init()
+
+
 def doSine(per=100, dt=0.01, ncycles=None):
+    dotstar = simpleDotStar(turnoff=True)  # default class, turn off
+    dotstar.deinit()
     da = machine.DAC(0)
     ct = 0
     while (not ncycles) or ct < ncycles:
