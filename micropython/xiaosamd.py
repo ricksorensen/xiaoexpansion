11e69fd19d9fa5af5597524097d6b3ddf5e234e6
Rick Sorensen <rick.sorensen@gmail.com>
2023-09-03 14:00:42 -0500

    xiaosamd.py:uPYv1.21(early) adapt to i2c and ssd1306 problem

--- micropython/xiaosamd.py
+++ micropython/xiaosamd.py
@@ -7,12 +7,18 @@ DAC_PIN = "A0_D0"
 ADC_PIN = "A2_D2"
 MYADCRESOLUTION = 12
 
+_SAMD_I2C_FREQ = 375000  # v 1.21 with select seems to have an issue
+# with ssd130 and i2c at some frequencies
+# SoftI2C works, f outside of [393443,400000] work
 SIGNAL_BUZZOFF = "A10_D10"
 SIGNAL_USEGPS = "A9_D9"
 
 # I2CSCL = "A5_D5"
 # I2CSDA = "A4_D4"
-i2c = machine.I2C(0, scl=machine.Pin("A5_D5"), sda=machine.Pin("A4_D4"), freq=400000)
+# i2c = machine.SoftI2C(scl=machine.Pin("A5_D5"), sda=machine.Pin("A4_D4"))
+i2c = machine.I2C(
+    0, scl=machine.Pin("A5_D5"), sda=machine.Pin("A4_D4"), freq=_SAMD_I2C_FREQ
+)
 
 # SERRX = "A6_D6"
 # SERTX = "A7_D7"
